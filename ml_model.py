3737df8bdfe99070eb08a698b85a1524a4e2b814
Satya13579 <satya.mahapatra94@gmail.com>
2022-12-27 22:41:31 +0530

    Added 3 new Ml Model Code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("Code related to ML Model")
+
+def model1():
+    print("Adding Model1 Algrthm")
+
+def model2():
+    print("Adding Model2 Algrthm")
+
+def model3():
+    print("Adding Model3 Algrthm")
